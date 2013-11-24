2e36b85c006739a0804c6aab1a806d772cbe74d4
Ahmed Soubky <ahmedsoubky@gmail.com>
2013-11-24 17:01:39 +0200

    Create types.h

---
+++ types.h
@@ -0,0 +1,16 @@
+typedef unsigned char boolean;
+typedef unsigned char u_int8;
+typedef signed char s_int8;
+typedef unsigned int u_int16;
+typedef signed int s_int16;
+typedef unsigned long u_int32;
+typedef signed long s_int32;
+typedef float float32;
+typedef double float64;
+
+#define BYTE s_int8
+#define UBYTE u_int8
+#define WORD s_int16
+#define UWORD u_int16
+#define LONG s_int32
+#define ULONG u_int32
