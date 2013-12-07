6563455213ce1f141215ab0ccbbf78f674e71bbc
Ahmed Soubky <ahmedsoubky@gmail.com>
2013-12-07 14:41:03 +0200

    Update types.h

--- types.h
+++ types.h
@@ -1,16 +1,16 @@
-typedef unsigned char boolean;
-typedef unsigned char u_int8;
-typedef signed char s_int8;
-typedef unsigned int u_int16;
-typedef signed int s_int16;
-typedef unsigned long u_int32;
-typedef signed long s_int32;
-typedef float float32;
-typedef double float64;
+typedef unsigned char BOOLEAN;
+typedef unsigned char UINT8;
+typedef signed char SINT8;
+typedef unsigned int UINT16;
+typedef signed int SINT16;
+typedef unsigned long UINT32;
+typedef signed long SINT32;
+typedef float FLOAT32;
+typedef double FLOAT64;
 
-#define BYTE s_int8
-#define UBYTE u_int8
-#define WORD s_int16
-#define UWORD u_int16
-#define LONG s_int32
-#define ULONG u_int32
+#define BYTE SINT8
+#define UBYTE UINT8
+#define WORD SINT16
+#define UWORD UINT16
+#define LONG SINT32
+#define ULONG UINT32
